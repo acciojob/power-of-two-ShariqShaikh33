fcb5ceb3795eff8a236bd2304e83aedc46f7c941
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-05-10 21:58:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const isPowerOfTwo = (number) => {
   //your code here
+	while(number>1){
+		number=number/2;
+	}
+	if(number==1){
+		return true;
+	}
+	return false;
 };
-
+ 
 const num = parseInt(prompt("Enter a number"));
 alert(isPowerOfTwo(num));
